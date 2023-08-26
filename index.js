8a293e6f6a99be529c8bd794db2c7a4f4e6cbc27
Yara Musad <98974263+yimusad@users.noreply.github.com>
2023-08-26 16:07:30 -0400

    remove redundant code

--- index.js
+++ index.js
@@ -22,31 +22,6 @@ function deleteChildElements(parent) {
  * Skills used: DOM manipulation, for loops, template literals, functions
 */
 
-// // grab the element with the id games-container
-// const gamesContainer = document.getElementById("games-container");
-
-// // create a function that adds all data from the games array to the page
-// function addGamesToPage(games) {
-
-//     // loop over each item in the data
-
-
-//         // create a new div element, which will become the game card
-
-
-//         // add the class game-card to the list
-
-
-//         // set the inner HTML using a template literal to display some info 
-//         // about each game
-//         // TIP: if your images are not displaying, make sure there is space
-//         // between the end of the src attribute and the end of the tag ("/>")
-
-
-//         // append the game to the games-container
-
-// }
-
 // Grab the element with the id games-container
 const gamesContainer = document.getElementById("games-container");
 
